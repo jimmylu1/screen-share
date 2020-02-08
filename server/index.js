acf7706e5369db00710864215c65842aa49cc901
jimmylu1 <jimmy88lu@gmail.com>
2020-02-08 01:54:23 -0800

    need to figure out how to connect to room

--- server/index.js
+++ server/index.js
@@ -1,14 +1,56 @@
-// Import the express framework for our node server
 const express = require('express');
-// Import the path module from node to create absolute file paths for express static
 const path = require('path');
+const {key, secret, token, sid } = require('./config.js');
+const { connect } = require('twilio-video');
+// require('dotenv').load();
 
-// Instantiate the express server
+
+
 const app = express();
-// Set a constant for the port that our express server will listen on
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+
+var AccessToken = require('twilio').jwt.AccessToken;
+
+var VideoGrant = AccessToken.VideoGrant;
+
+
+// Substitute your Twilio AccountSid and ApiKey details
+var ACCOUNT_SID = sid;
+var API_KEY_SID = key;
+var API_KEY_SECRET = secret;
+
+// Create an Access Token
+var accessToken = new AccessToken(
+  ACCOUNT_SID,
+  API_KEY_SID,
+  API_KEY_SECRET
+);
+
+// Set the Identity of this token
+accessToken.identity = 'example-user';
+
+// Grant access to Video
+var grant = new VideoGrant();
+grant.room = 'cool room';
+accessToken.addGrant(grant);
+
+// Serialize the token as a JWT
+var jwt = accessToken.toJwt();
+console.log(jwt);
+
+connect(jwt, { name: 'my-new-room' }).then(room => {
+  console.log(`Successfully joined a Room: ${room}`);
+  room.on('participantConnected', participant => {
+    console.log(`A remote Participant connected: ${participant}`);
+  });
+}, error => {
+  console.error(`Unable to connect to Room: ${error.message}`);
+});
 
 // Serve static files. Any requests for specific files will be served if they exist in the provided folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+
 // Start the server on the provided port
 app.listen(PORT, () => console.log('Listening on port: ' + PORT));
