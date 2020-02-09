ecd52259d1ced7b6e44b502f24862cb6e2597feb
jimmylu1 <jimmy88lu@gmail.com>
2020-02-08 17:06:43 -0800

    complete api endpoint to access token

--- server/index.js
+++ server/index.js
@@ -1,56 +1,37 @@
-const express = require('express');
-const path = require('path');
-const {key, secret, token, sid } = require('./config.js');
-const { connect } = require('twilio-video');
-// require('dotenv').load();
+const express = require("express");
+const twilio = require("twilio");
+const { key, secret, roomToken, sid } = require('./config.js');
 
 
+const AccessToken = twilio.jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
+
+const port = process.env.PORT || 3000;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-
-var AccessToken = require('twilio').jwt.AccessToken;
-
-var VideoGrant = AccessToken.VideoGrant;
-
-
-// Substitute your Twilio AccountSid and ApiKey details
-var ACCOUNT_SID = sid;
-var API_KEY_SID = key;
-var API_KEY_SECRET = secret;
-
-// Create an Access Token
-var accessToken = new AccessToken(
-  ACCOUNT_SID,
-  API_KEY_SID,
-  API_KEY_SECRET
-);
-
-// Set the Identity of this token
-accessToken.identity = 'example-user';
-
-// Grant access to Video
-var grant = new VideoGrant();
-grant.room = 'cool room';
-accessToken.addGrant(grant);
-
-// Serialize the token as a JWT
-var jwt = accessToken.toJwt();
-console.log(jwt);
-
-connect(jwt, { name: 'my-new-room' }).then(room => {
-  console.log(`Successfully joined a Room: ${room}`);
-  room.on('participantConnected', participant => {
-    console.log(`A remote Participant connected: ${participant}`);
-  });
-}, error => {
-  console.error(`Unable to connect to Room: ${error.message}`);
+app.listen(port, function () {
+  console.log('Server running on: ' + port);
 });
 
-// Serve static files. Any requests for specific files will be served if they exist in the provided folder
-app.use(express.static(path.join(__dirname, '../client/dist')));
+// use /token endpoint to get an access token that will let us make the call
+app.get("/token", (req, res) => {
+  var ACCOUNT_SID = sid;
+  var API_KEY_SID = key;
+  var API_KEY_SECRET = secret;
+  //generate new token
+  const token = new AccessToken(
+    ACCOUNT_SID,
+    API_KEY_SID,
+    API_KEY_SECRET
+  );
+  //grant acess to token
+  token.addGrant(new VideoGrant());
+
+  token.identity = req.query.user;
+
+  //send back to client
+  res.send({ token: token.toJwt() });
+});
 
 
-// Start the server on the provided port
-app.listen(PORT, () => console.log('Listening on port: ' + PORT));
