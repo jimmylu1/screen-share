e60fd1dbd7b2a838e34561003475ab3fefecf726
jimmylu1 <jimmy88lu@gmail.com>
2020-02-08 19:30:50 -0800

    join a room implemented

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -1,17 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import adapter from "webrtc-adapter";
 import TwilioVideo from "twilio-video";
-import { getToken } from './api.js'
-
+import { getToken } from "./api.js";
 
 export default class Container extends Component {
-  constructor(){
+  constructor() {
     state = {
       videoRoom: null,
       joined: false,
-      userName: '',
-      roomName: ''
-    }
+      username: "",
+      roomName: ""
+    };
   }
 
   componentDidMount() {
@@ -25,8 +24,46 @@ export default class Container extends Component {
     const { username } = this.state;
     const res = await getToken(username);
     return res.data.token;
-  }
+  };
 
   //join a room
-  
+  joinRoom = async () => {
+    const { roomName } = this.state;
+    //update joined state to true
+    this.setState({ joined: true });
+
+    try {
+      const token = await this.getToken();
+
+      //get local video stream
+      const localVideoTrack = await TwilioVideo.createLocalVideoTrack();
+      this.setState({ localVideoTrack });
+
+      //get local audo stream
+      const localAudioTrack = await TwilioVideo.createLocalAudioTrack();
+      this.setState({ localAudioTrack });
+
+      //new video room with generated token
+      const videoRoom = await TwilioVideo.connect(token, {
+        name: roomName,
+        tracks: [localVideoTrack, localAudioTrack],
+        insights: false
+      });
+      //if disconnected, stop video, audio, screen share
+      videoRoom.on("disconnected", () => {
+        this.stopVideoTrack();
+        this.stopAudioTrack();
+        this.stopScreenTrack();
+
+        this.setState({
+          videoRoom: null
+        });
+      });
+      this.setState({ videoRoom });
+    } catch (err) {
+      this.stopVideoTrack();
+      this.stopAudioTrack();
+    }
+    this.setState({ joined: false });
+  };
 }
