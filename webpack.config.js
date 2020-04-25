26c034d6757c8269504430df51921a1f83a68b42
jimmylu1 <jimmy88lu@gmail.com>
2020-04-24 21:58:29 -0700

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: "bundle.js",
-    path: DIST_DIR
+    path: DIST_DIR,
+    publicPath: "/"
   },
   module: {
     rules: [
