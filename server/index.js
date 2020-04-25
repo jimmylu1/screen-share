ecc6948c14af9b1ba4b1d8c38a518713e0ba2185
jimmylu1 <jimmy88lu@gmail.com>
2020-04-24 20:20:54 -0700

    add heroku config

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const twilio = require("twilio");
 const path = require("path");
 
-const PORT = 3000;
+const PORT = process.env.port || 3000;
 
 const app = express();
 app.use(express.static(path.join(__dirname, "../client/dist")));
